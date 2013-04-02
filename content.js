48cc0425e374a5d11c833595316c72360ff8c5dc
Bernard McKeever <dregin@gmail.com>
2013-04-03 00:02:10 +0100

    Groundwork
    Add button to daft.ie page.
    
    TODO:
    - Create empty div.
    - Attach event to button
    - Get info from dropdaft.com on click
    - Populate empty div with slide down animation below daft.ie price div

---
+++ content.js
@@ -0,0 +1,18 @@
+var DropButton = '<span class=\'bar\'>|</span><button id=\'dropButton\' type=\'button\'>Daft Drop!</button>';
+
+$('#smi-summary-items').append(DropButton);
+
+
+$('#smi-object-info').after('<div>TEST DIV TEST DIV TEST DIV</div>');
+
+
+
+$('#smi-summary-items'); 
+<div id="smi-summary-items">
+  <div>€195,000</div>
+  <span class="header_text">Apartment for sale</span>
+  <span class="bar">|</span>
+  <span class="header_text">3 Bedrooms</span>
+  <span class="bar">|</span>
+  <a style="font-weight: normal;" href="aib/">Finance with AIB Mortgages</a>
+</div>
