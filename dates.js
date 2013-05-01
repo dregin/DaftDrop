d5bee227239787b07cedb35107a9ee706945c89a
Bernard McKeever <dregin@gmail.com>
2013-05-01 01:17:29 +0100

    Javascript to generate date from passed encoded string
    GWT craziness.

---
+++ dates.js
@@ -0,0 +1,63 @@
+a = 'TNhk0AA';
+
+var OW = null;
+var eW = {"d":"[Lcom.google.gwt.lang.LongLibBase$LongEmul;","c":4};
+var yL = ["gC","eQ","hC","tS","toString","tM","cM"];
+var zL = [null,null,null,null,null,null,{}];
+//var nX = {"l":0,"m":0,"h":524288};
+var pX = {"l":0,"m":0,"h":0};
+
+function Ywb(){}
+function sX(){}
+function AL(){return Ywb()}
+//function qX(){return Ywb()}
+function DW(a,b,c){return _=new sX,_.l=a,_.m=b,_.h=c,_}
+function CL(a,b,c){AL();for(var d=0,e=b.length;d<e;++d){a[b[d]]=c[d]}}
+
+function wL(a,b,c,d){AL();CL(d,yL,zL);d.aC=a;d.cM=b;d.qI=c;return d}
+function tL(a,b){var c=new Array(b);if(a==3){for(var d=0;d<b;++d){var e=new Object;e.l=e.m=e.h=0;c[d]=e}}else if(a>0){var e=[null,0,false][a];for(var d=0;d<b;++d){c[d]=e}}return c}
+function vL(a,b,c,d,e){var f;f=tL(e,d);wL(a,b,c,f);return f}
+function BW(a){var b,c,d;b=a&4194303;c=~~a>>22&4194303;d=a<0?1048575:0;return DW(b,c,d)}
+function WW(a){var b,c;if(a>-129&&a<128){b=a+128;OW==null&&(OW=vL(eW,{96:1,111:1},58,256,0));c=OW[b];!c&&(c=OW[b]=BW(a));return c}return BW(a)}
+
+
+function qX() {
+    qX = Ywb;
+    mX = DW(4194303, 4194303, 524287);
+    nX = DW(0, 0, 524288);
+    oX = WW(1);
+    WW(2);
+    pX = WW(0)
+}
+
+function UW(a,b){return a.l==b.l&&a.m==b.m&&a.h==b.h}
+function YW(a,b){var c,d;c=~~a.h>>19;d=~~b.h>>19;return c==0?d!=0||a.h>b.h||a.h==b.h&&a.m>b.m||a.h==b.h&&a.m==b.m&&a.l>=b.l:!(d==0||a.h<b.h||a.h==b.h&&a.m<b.m||a.h==b.h&&a.m==b.m&&a.l<b.l)}
+function bX(a){var b,c,d;b=~a.l+1&4194303;c=~a.m+(b==0?1:0)&4194303;d=~a.h+(b==0&&c==0?1:0)&1048575;return DW(b,c,d)}
+function MW(a){return a.l+a.m*4194304+a.h*17592186044416}
+
+function iX(a) {
+    if (UW(a, (qX(), nX))) {
+        return -9223372036854775808
+    }
+    if (!YW(a, pX)) {
+        return -MW(bX(a))
+    }
+    return a.l + a.m * 4194304 + a.h * 17592186044416
+}
+
+function bv(a){return new Date(a)}
+
+function ppb(b,a){return b.charCodeAt(a)}
+function SW(a){if(a>=65&&a<=90){return a-65}if(a>=97){return a-97+26}if(a>=48&&a<=57){return a-48+52}if(a==36){return 62}return 63}
+function dX(a,b){var c,d,e;b&=63;if(b<22){c=a.l<<b;d=a.m<<b|~~a.l>>22-b;e=a.h<<b|~~a.m>>22-b}else if(b<44){c=0;d=a.l<<b-22;e=a.m<<b-22|~~a.l>>44-b}else{c=0;d=0;e=a.l<<b-44}return DW(c&4194303,d&4194303,e&1048575)}
+function cX(a,b){return DW(a.l|b.l,a.m|b.m,a.h|b.h)}
+function ZW(a){var b,c,d;d=0;c=WW(SW(ppb(a,d++)));b=a.length;while(d<b){c=dX(c,6);c=cX(c,WW(SW(ppb(a,d++))))}return c}
+
+function vK(a) {
+	a = ZW(a);
+    return bv(iX(a))
+}
+
+function getDate(dateString){
+	return vK(dateString)
+}
